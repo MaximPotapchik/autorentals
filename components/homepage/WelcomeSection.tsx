99f0c4a9da7bac4b2886405e5cf3412f044c132e
Maxim <maximp1012@gmail.com>
2023-04-20 14:26:35 -0400

    carousel and some text

--- components/homepage/WelcomeSection.tsx
+++ components/homepage/WelcomeSection.tsx
@@ -1,3 +1,5 @@
+import AboveCarouselText from "./middle/AboveCarouselText";
+import CarCarousel from "./middle/CarCarousel";
 import ExpandingBoxes from "./middle/ExpandingBoxes";
 import TextBelowImage from "./top/TextBelowImage";
 import TopImage from "./top/TopImage";
@@ -5,9 +7,11 @@ import TopImage from "./top/TopImage";
 const WelcomeSection = () => {
     return ( 
     
-        <div>
+        <div className="flex flex-col p-2 bg-wh">
             <TopImage />
             <TextBelowImage />
+            <AboveCarouselText />
+            <CarCarousel />
             <ExpandingBoxes />
         </div>
     
